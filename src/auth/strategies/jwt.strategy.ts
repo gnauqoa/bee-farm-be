f6105a1c3cae734d32e61948e89ee6434ddbcba4
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2025-03-26 10:11:14 +0700

    refactor: jwt strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -5,23 +5,30 @@ import { ConfigService } from '@nestjs/config';
 import { OrNeverType } from '../../utils/types/or-never.type';
 import { JwtPayloadType } from './types/jwt-payload.type';
 import { AllConfigType } from '../../config/config.type';
+import { UserRepository } from '../../users/infrastructure/persistence/user.repository';
+import { User } from '../../users/domain/user';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(configService: ConfigService<AllConfigType>) {
+  constructor(
+    configService: ConfigService<AllConfigType>,
+    private readonly userRepository: UserRepository,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
       secretOrKey: configService.getOrThrow('auth.secret', { infer: true }),
     });
   }
 
   // Why we don't check if the user exists in the database:
   // https://github.com/brocoders/nestjs-boilerplate/blob/main/docs/auth.md#about-jwt-strategy
-  public validate(payload: JwtPayloadType): OrNeverType<JwtPayloadType> {
-    if (!payload.id) {
-      throw new UnauthorizedException();
+  public async validate(payload: JwtPayloadType): Promise<OrNeverType<User>> {
+    const user = await this.userRepository.findById(payload.id);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
     }
 
-    return payload;
+    return user;
   }
 }
